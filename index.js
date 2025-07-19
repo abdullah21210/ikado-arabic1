6977820b039544b796a5abc65748ddfffb4aeea7
عبدالله 21210 <a0507511167@gmail.com>
2025-07-20 00:11:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// نموذج نهائي لمحاكي أوتوكاد + Office عربي متكامل (2D + Word + Excel + PowerPoint + حفظ + طبقات + PNG/PDF + 3D + ذكاء + نشر ويب + صوت + اقتراحات + مكتبة أشكال + إعدادات + حماية بكلمة مرور + تسجيل دخول)
+
+import React, { useEffect, useRef, useState } from "react";
+import { fabric } from "fabric";
+import * as THREE from "three";
+import jsPDF from "jspdf";
+
+// ... (بقية الشيفرة مأخوذة من الكانفاس الذي تم حفظه)
